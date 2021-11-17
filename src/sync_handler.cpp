fec5ab4e7648bcd9a792aa55bb502d80b6fba0ea
Martin <mchri17@student.aau.dk>
2021-11-17 22:29:43 +0100

    new try

--- src/sync_handler.cpp
+++ src/sync_handler.cpp
@@ -4,13 +4,69 @@
 
 #include "sync_handler.h"
 
-SYNC_handler::SYNC_handler() {
+
+SYNC_handler::SYNC_handler(int number_of_slaves, char** slaves_ips) {
+    this->number_of_slaves = number_of_slaves;
+    this->slave_ips = slaves_ips;
+    this->slaves = new Sync_Master[number_of_slaves];
+    setup();
+
 
 }
 
 void SYNC_handler::run() {
 
-    for (int i = 0; i < 1000; i++) {
-        //
+    reset_time();
+    synchronize();
+}
+
+
+
+int SYNC_handler::setup() {
+    int err;
+
+    for (int i = 0; i < number_of_slaves; i++) {
+        err = add_slave(i ,slave_ips[i], true);
+        if(err < 0){
+            std::cout << "Slave " << i << " did not setup, [SZS_server, setup()]" << std::endl;
+        }
     }
+    return 0;
+}
+
+
+int SYNC_handler::add_slave(int slave_number, char* host, bool is_ip) {
+    if (slave_number >= number_of_slaves){
+        std::cout << "There can only be 8 slaves, [SZS_server, add_slave(int slave_number, char* host, bool is_ip)]" << std::endl;
+        return -1;
+    }
+    slaves[slave_number] = Sync_Master(host, is_ip);
+
+
+    return 0;
+}
+
+void SYNC_handler::reset_time(){
+    for (int i = 0; i < number_of_slaves; i++) {
+        slaves[i].Reset_Time();
+    }
+}
+
+
+int SYNC_handler::synchronize(){
+
+    for (int i = 0; i < number_of_slaves; i++) {
+        jump :
+
+        std::cout << "lige før ACCp" << std::endl;
+        slaves[i].SyncReq_and_accept();
+        std::cout << "ACCp" << std::endl;
+        slaves[i].Send_TS23();
+        std::cout << "Burde have sent" << std::endl;
+        if (!slaves[i].Wait_for_Sync_OK()){
+            goto jump;
+        }
+        std::cout<<"Successful sync to client: "<< i+1 << std::endl;
+    }
+    return 0;
 }
