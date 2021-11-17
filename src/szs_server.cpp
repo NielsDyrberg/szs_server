c9650212f0d4a3df9930c9eb6ecf3ce46d5c1fdd
Niels Dyrberg <ncpd@live.dk>
2021-11-17 23:10:01 +0100

    Debug handles added

--- src/szs_server.cpp
+++ src/szs_server.cpp
@@ -6,7 +6,7 @@
 
 #include "szs_server.h"
 
-static char* slave_ips[] = {"192.168.1.46", "192.168.1.10", nullptr, nullptr,
+static char* slave_ips[] = {"192.168.1.46", "192.168.1.48", nullptr, nullptr,
                             nullptr, nullptr, nullptr, nullptr};
 
 /**********************************************************************************************************************
